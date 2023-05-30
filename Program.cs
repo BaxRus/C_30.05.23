106472ce88f0c7e684e65724ff01da8a7599a9ec
Dmitrij Voznyak <dv98dv@ya.ru>
2023-05-30 23:41:58 +0600

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿// Семинар от 30.05.23 Сишарп
+﻿// Задача 24: Напишите программу, которая
+//  принимает на вход число (А) и выдаёт сумму
+//   чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("введите число");
+int num =Convert.ToInt32(Console.ReadLine());
+
+int GetSumNumber()
+{
+    int sum=0;
+    int count =1;
+
+    while (count<=num)
+    {
+        sum= sum+count;
+        count=count+1;
+    }
+
+    return sum;
+}
+int result=GetSumNumber();
+Console.WriteLine(result);
